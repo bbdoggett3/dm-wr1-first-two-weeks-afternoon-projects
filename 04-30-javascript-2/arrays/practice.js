a3e64dad48619dde1444c36295bf581491b4bb24
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-07 21:33:33 -0600

    worked on the extra problems in array for pratice

--- 04-30-javascript-2/arrays/practice.js
+++ 04-30-javascript-2/arrays/practice.js
@@ -18,7 +18,7 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+let first = (arr) => arr[0];
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +33,7 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+let last = (arr) => arr[2]; //I wanted to write with a arrow function and get comfortable with them.
 
 
 ////////// PROBLEM 3 //////////
@@ -48,6 +48,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(family) {
+  for( let i=0; i < family.length; i++) {
+    alert(family[i]);
+  }
+}
 
 
 
@@ -63,6 +68,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters) {
+  for (let i = letters.length -1; i >= 0; i--) {
+    console.log(letters[i])
+    alert(letters[i]);
+  }
+}
 
 
 
@@ -78,6 +89,17 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(nums) {
+  let evenNum = [];
+
+  for(let i = 0; i < nums.length; i++) {
+    console.log(nums[i])
+    if(nums[i] % 2 === 0) {
+      evenNum.push(nums[i]);
+    }
+  }
+  return evenNum;
+}
 
 
 
@@ -107,6 +129,18 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
+function divider(numbersArray) {
+  let evens = numbersArray.filter(function(nums) {
+    return nums % 2 === 0;
+  });
+  let odds = numbersArray.filter(function(nums) {
+    return nums % 2 !== 0;
+  })
+
+  console.log(numbersArray)
+  return [evens, odds];
+}
+
 
 
 
@@ -129,6 +163,16 @@ var getRandomArbitrary = function() {
 */
 
 //Code Here
+function finder(arr) {
+  let randomNumber = getRandomArbitrary();
+
+  for( let i = 0; i < arr.length; i++) {
+    if(randomNumber === arr[i]) {
+      return true;
+    }
+  }
+  return false;
+}
 
 
 
@@ -158,6 +202,19 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 */
 
 //Code Here
+function removeItem(myGroceryList, item) {
+  for(let i = 0; i < myGroceryList.length; i++ ) {
+    if(myGroceryList[i] === item) {
+      console.log(myGroceryList)
+      myGroceryList.splice(i, 0)
+    }
+  }
+  console.log(myGroceryList)
+return myGroceryList;
+}
+
+removeItem(myGroceryList, "chips");
+
 
 
 
